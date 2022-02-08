f619015f511e1592f03c972935163ed42cd08759
Sagar Srinivas <68839417+sagarsrinivas3@users.noreply.github.com>
2022-02-08 20:51:33 +0000

    added sendMessageWithInterval block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import yagmail
+import time
 
 sender   = "sample.00.email@gmail.com"
 recevier = "bigtvindia@gmail.com" #bigtvindia@gmail.com
@@ -22,9 +23,19 @@ def sendMessage(mailObj, recvr, sub, cont):
   mailObj.send(to=recevier, subject=subject, contents=content)
   print("EMAIL SENT!")
 
+def sendMessageWithInterval(mailObj, recvr, sub, cont, interval):
+  while True:
+      mailObj.send(to=recevier, subject=subject, contents=content)
+      time.sleep(interval)
+      print("EMAIL SENT!")
+    
+  
+
 
 mailObj = getMailObj(sender, password)
 sendMessage(mailObj, recevier, subject, content)
+interval = 3
+sendMessageWithInterval(mailObj, recevier, subject, content, interval)
 
 
 
