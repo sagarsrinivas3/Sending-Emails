06fc84f282b1066a3cd64fba717e2dedf7a36d44
Sagar Srinivas <68839417+sagarsrinivas3@users.noreply.github.com>
2022-02-09 18:06:17 +0000

    send mail using hotmail

--- sendmail.py
+++ sendmail.py
@@ -1,1 +1,18 @@
-import 
+import smptlib
+import os
+
+sender = "anything@hotmail.com"
+recevier = ""
+pass = ""
+
+message = """\
+            Subject : Lets Start 
+
+            This is sagar, welcome!!
+          """
+
+server = smptlib.SMPT('smpt.office365.com', 587)
+server.starttls()
+server.login(sender, pass)
+server.sendmail(sender, recevier, message)
+server.quit()
