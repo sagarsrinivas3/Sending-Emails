a9cb9e9e156a8ef4499e4ed8f19d69527aeb96a6
Sagar Srinivas <68839417+sagarsrinivas3@users.noreply.github.com>
2022-02-09 16:39:03 +0000

    contentWithAttachment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import yagmail
 import time
 from datetime import datetime as dt
+import pandas
 
 sender   = "sample.00.email@gmail.com"
 recevier = "bigtvindia@gmail.com" #bigtvindia@gmail.com
@@ -27,8 +28,9 @@ def sendMessage(mailObj, recvr, sub, cont):
 def sendMessageWithInterval(mailObj, recvr, sub, cont, interval):
   while True:
       mailObj.send(to=recevier, subject=subject, contents=content)
-      time.sleep(interval)
       print("EMAIL SENT!")
+      time.sleep(interval)
+
 
 def sendMessageAtCertainTime(mailObj, recvr, sub, cont, hr, min):
   while True:
@@ -37,18 +39,43 @@ def sendMessageAtCertainTime(mailObj, recvr, sub, cont, hr, min):
         mailObj.send(to=recevier, subject=subject, contents=content)
         print("EMAIL SENT!")
         time.sleep(60)
-       
+
+
+def sendMessageMultipleRecivers(mailObj, recvrs, sub, cont):
+  for recr in recvrs:
+      mailObj.send(to=recr, subject=subject, contents=content)
+      print("EMAIL SENT!")
+      time.sleep(2)
     
   
 
 
 mailObj = getMailObj(sender, password)
+
 sendMessage(mailObj, recevier, subject, content)
+
 interval = 3
 sendMessageWithInterval(mailObj, recevier, subject, content, interval)
+
 hour = "21"
 minute = "8"
 sendMessageAtCertainTime(mailObj, recevier, subject, content, hour, minute)
+
+df = pandas.read_csv('contacts.csv')
+email_list = []
+for index, row in df.iterrows():
+  email_list.append(row['email'])
+sendMessageMultipleRecivers(mailObj, email_list, subject, content)
+
+# send email with attachments
+contentWithAttachment = ["""
+Here is the content of the email!!
+How are you!!?
+Thanks
+Your Bot1
+""", 'attachment.txt']
+sendMessage(mailObj, recevier, subject, contentWithAttachment)
+
 
 
 
